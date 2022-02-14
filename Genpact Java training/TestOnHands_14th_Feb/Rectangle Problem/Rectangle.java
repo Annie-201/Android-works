f9465b996cdc0d7bf7fbaa2750b9e7bca5e7e1a8
Annie-201 <63703623+Annie-201@users.noreply.github.com>
2022-02-14 13:21:13 +0530

    test
    Test

---
+++ Genpact Java training/TestOnHands_14th_Feb/Rectangle Problem/Rectangle.java
@@ -0,0 +1,16 @@
+package TestOnHands;
+
+public class Rectangle
+{
+   private double length;  
+   private double width;   
+   public void setRectangle(double length, double width)
+   {
+      this.length = length;
+      this.width  = width;
+   }
+   public double getArea()
+   {
+      return length * width;
+   }
+}
